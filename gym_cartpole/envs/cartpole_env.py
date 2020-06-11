4c22f3ba99f7db93cb9a8274ed3a139c240be98a
marc131183 <marcg99@zedat.fu-berlin.de>
2020-06-11 13:31:03 +0200

    changed starting position

--- gym_cartpole/envs/cartpole_env.py
+++ gym_cartpole/envs/cartpole_env.py
@@ -101,9 +101,7 @@ class CartpoleEnv(gym.Env):
         return np.array(self.state), reward, done, {}
     
     def reset(self):
-        self.state = np.append(self.np_random.uniform(low=-0.20, high=0.20, size=(2,)), np.array([0,0]))
-        print(self.np_random.uniform(low=-0.20, high=0.20, size=(4,)))
-        print(np.append(self.np_random.uniform(low=-math.pi, high=math.pi, size=(2,)), np.array([0,0])))
+        self.state = np.append(self.np_random.uniform(low=-0.20, high=0.20, size=(2,)), np.array([2*math.pi,2*math.pi]))
         self.steps_beyond_done = None
         return np.array(self.state)
 
